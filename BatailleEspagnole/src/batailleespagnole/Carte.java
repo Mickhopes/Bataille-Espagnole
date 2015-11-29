85b22b44d46142532299d09a9a177a6172ee5a0b
LilyOfTheWest <linovergame@hotmail.fr>
2015-11-29 20:52:52 +0100

    Javadoc Joueur et Carte + Main=BatailleEspagnole

--- BatailleEspagnole/src/batailleespagnole/Carte.java
+++ BatailleEspagnole/src/batailleespagnole/Carte.java
@@ -3,8 +3,16 @@ package batailleespagnole;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.6384B198-9A51-156D-F380-21276E467CE3]
 // </editor-fold> 
+/**
+ * Classe servant à représenter une carte de jeu
+ * 
+ * @author Line
+ */
 public class Carte implements Comparable<Carte> {
 
+    /**
+     * Enumération correspondant aux différents ordres possibles d'une carte de jeu
+     */
     static enum TypeOrdre {
 
         DEUX, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF, DIX, CAVALIER, DAME, ROI, TROIS, AS;
@@ -13,15 +21,37 @@ public class Carte implements Comparable<Carte> {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.94E54F8B-922C-A362-FFC7-336B808D8B63]
     // </editor-fold> 
+    /** 
+     * L'ordre d'une carte de jeu
+     * 
+     * @see TypeOrdre
+     */
     private TypeOrdre ordre;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.EB3D496D-A648-B074-ECDB-8BFBD8AA17D5]
     // </editor-fold> 
+    /**
+     * Valeur d'une carte en terme de points
+     */
     private int valeur;
 
+    /**
+     * Famille de la carte
+     * 
+     * @see TypeFamille
+     */
     private TypeFamille famille;
 
+    /**
+     * Constructeur de Carte.
+     * 
+     * @param ordre L'ordre de la carte
+     * @param famille La famille de la carte
+     * 
+     * @see TypeOrdre
+     * @see TypeFamille
+     */
     public Carte(TypeOrdre ordre, TypeFamille famille) {
         this.ordre = ordre;
         this.famille = famille;
@@ -59,6 +89,11 @@ public class Carte implements Comparable<Carte> {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.4B0892EA-7CCC-BD9C-0057-36BCE26BA1F5]
     // </editor-fold> 
+    /**
+     * Retourne l'ordre d'une carte.
+     * 
+     * @return Une énumération TypeOrdre qui correspond à l'ordre de la carte
+     */
     public TypeOrdre getOrdre() {
         return ordre;
     }
@@ -66,6 +101,11 @@ public class Carte implements Comparable<Carte> {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.AFF1BB55-8CB5-1724-8640-68D544841CB7]
     // </editor-fold> 
+    /**
+     * Met à jour l'ordre d'une carte.
+     * 
+     * @param val L'ordre à mettre à jour sur la carte
+     */
     public void setOrdre(TypeOrdre val) {
         this.ordre = val;
     }
@@ -73,6 +113,11 @@ public class Carte implements Comparable<Carte> {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.B1A3DC70-FE6B-0E95-DB5D-D9472652FCAF]
     // </editor-fold> 
+    /**
+     * Retourne la valeur en points d'une carte.
+     * 
+     * @return Un entier qui correspond à la valeur de la carte
+     */
     public int getValeur() {
         return valeur;
     }
@@ -80,18 +125,39 @@ public class Carte implements Comparable<Carte> {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.1F2E7405-F0A5-31D5-43FD-4A200F91DABE]
     // </editor-fold> 
+    /**
+     * Met à jour la valeur d'une carte en points.
+     * 
+     * @param val Un entier pour mettre à jour la valeur
+     */
     public void setValeur(int val) {
         this.valeur = val;
     }
 
+    /**
+     * Retourne la famille d'une carte.
+     * 
+     * @return Une énumération TypeFamille qui correspond à la famille d'une carte
+     */
     public TypeFamille getFamille() {
         return famille;
     }
 
+    /**
+     * Met à jour la famille d'une carte.
+     * 
+     * @param famille Le type énumération TypeFamille
+     */
     public void setFamille(TypeFamille famille) {
         this.famille = famille;
     }
 
+    /**
+     * Permet de comparer deux Cartes entre elles
+     * 
+     * @param c Une carte
+     * @return Un entier : 1 si c est plus grande en ordre que l'objet actuel,0 si elles sont de mêmes ordre et -1 sinon. 
+     */
     @Override
     public int compareTo(Carte c) {
         if (c.getFamille() == this.famille) {
@@ -100,6 +166,11 @@ public class Carte implements Comparable<Carte> {
         return -1;
     }
 
+    /**
+     * Permet de formater l'affichage d'une Carte.
+     * 
+     * @return La chaine qui affichera l'objet Carte
+     */
     @Override
     public String toString() {
         String s = "";
--- BatailleEspagnole/src/batailleespagnole/Joueur.java
+++ BatailleEspagnole/src/batailleespagnole/Joueur.java
@@ -3,6 +3,11 @@ package batailleespagnole;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Classe servant à représenter un joueur
+ * 
+ * @author Line
+ */
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.CC54FE1A-D482-F1F8-4BC2-0DB57B57C53A]
 // </editor-fold> 
@@ -11,31 +16,67 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D6EBBA9C-A2DF-2EC7-ADDC-140CC30BCF12]
     // </editor-fold> 
+    /**
+     * Le nom du joueur
+     */
     private String nom;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.078D6297-7935-837D-41DE-9C3839B0AA87]
     // </editor-fold> 
+    /**
+     * Le nombre de points du joueur durant la partie
+     */
     private int nbPoints;
 
+    /**
+     * Attribut qui permet d'indiquer si ce joueur joue en premier
+     */
     private boolean premier;
 
+    /**
+     * Attribut qui permet d'indiquer si ce joueur est un IA
+     */
     private boolean IA;
     
+    /**
+     * Attribut qui permet de savoir si c'est au tour du joueur
+     */
     private boolean tour;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.BE6D8EFC-C7C8-5E18-76BD-752E80A886A7]
     // </editor-fold> 
+    /**
+     * ArrayList de Carte qui correspondent aux cartes dans la main du joueur
+     * 
+     * @see Carte
+     */
     private ArrayList<Carte> cartesEnMain;
 
+    /**
+     * ArrayList de Carte qui correspondent aux cartes qu'il a gagné durant le jeu
+     *
+     * @see Carte
+     */
     private ArrayList<Carte> plisGagnes;
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.B1CC00A2-ECB6-67A4-6437-8C82AF977CFB]
     // </editor-fold> 
+    /**
+     * Attribut qui correspond à la partie à laquelle le joueur participe
+     * 
+     * @see Partie
+     */
     private Partie mPartie;
 
+    /**
+     * Constructeur de joueur
+     * 
+     * @param nom Le nom du joueur
+     * @param IA Indique si le joueur est une IA ou pas
+     */
     public Joueur(String nom, boolean IA) {
         this.nom = nom;
         this.nbPoints = 0;
@@ -49,6 +90,11 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.5B763AA0-337C-FCC8-7B08-FF7C4E142B78]
     // </editor-fold> 
+    /** 
+     * Retourne la partie à laquelle le joueur participe
+     * 
+     * @return Partie
+     */
     public Partie getPartie() {
         return mPartie;
     }
@@ -56,6 +102,11 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.4348DC49-88EC-2A08-1E73-F709231262CF]
     // </editor-fold> 
+    /**
+     * Met à jour la partie à laquelle le joueur participe
+     * 
+     * @param val La Partie à lui attribuer
+     */
     public void setPartie(Partie val) {
         this.mPartie = val;
     }
@@ -63,6 +114,11 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.A75E7EE8-52B8-B972-69C2-271AB5C0C194]
     // </editor-fold> 
+    /**
+     * Retourne le nombre de points d'un joueur
+     * 
+     * @return Un entier qui correspond au nombre de points d'un joueur
+     */
     public int getNbPoints() {
         return nbPoints;
     }
@@ -70,6 +126,11 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.2CD0510C-C057-A5CB-1A97-01D965F2BFE2]
     // </editor-fold> 
+    /**
+     * Met à jour le nombre de points du joueur
+     * 
+     * @param val Un entier qui correspond au nombre de points à mettre à jour
+     */
     public void setNbPoints(int val) {
         this.nbPoints = val;
     }
@@ -77,6 +138,11 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.7DCDEA3A-8EFA-3FCC-6BA3-F7E2B34A9FE9]
     // </editor-fold> 
+    /**
+     * Retourne le nom du joueur
+     * 
+     * @return String qui correspond au nom du joueur
+     */
     public String getNom() {
         return nom;
     }
@@ -84,62 +150,141 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,regenBody=yes,id=DCE.E0A9BC8A-DD14-8DD5-6AED-1506876777BE]
     // </editor-fold> 
+    /**
+     * Met à jour le nom du joueur
+     * 
+     * @param val String qui correspond au nom du Joueur à mettre à jour
+     */
     public void setNom(String val) {
         this.nom = val;
     }
 
+    /**
+     * Méthode qui permet d'ajouter un certain nombre de points au joueur
+     * 
+     * @param val Un entier qui correspond au nombre de points à ajouter
+     */
     public void ajoutPoints(int val) {
         this.nbPoints += val;
     }
 
+    /**
+     * Indique si le joueur est le premier à jouer
+     * 
+     * @return boolean
+     */
     public boolean isPremier() {
         return premier;
     }
 
+    /**
+     * Met à jour le booléen qui indique si le joueur est le premier à jour
+     * 
+     * @param premier boolean
+     */
     public void setPremier(boolean premier) {
         this.premier = premier;
     }
 
+    /**
+     * Indique si c'est au tour du joueur de jouer
+     * 
+     * @return boolean
+     */
     public boolean isTour() {
         return tour;
     }
 
+    /**
+     * Met à jour le booléen qui indique si c'est au joueur de jouer
+     * 
+     * @param tour boolean
+     */
     public void setTour(boolean tour) {
         this.tour = tour;
     }
 
+    /**
+     * Retourne les cartes dans la main du joueur
+     * 
+     * @return ArrayList<Carte> Les cartes dans la main du joueur
+     * @see Carte
+     */
     public ArrayList<Carte> getCartesEnMain() {
         return cartesEnMain;
     }
 
+    /**
+     * Met à jour les cartes en main du joueur
+     * 
+     * @param cartesEnMain ArrayList<Carte> qui correspond aux cartes dans la main du joueur
+     * @see Carte
+     */
     public void setCartesEnMain(ArrayList<Carte> cartesEnMain) {
         this.cartesEnMain = cartesEnMain;
     }
 
+    /**
+     * Retourne les plis gagnés par le joueur
+     * 
+     * @return ArrayList<Carte> qui correspond aux cartes gagnées par le joueur
+     */
     public ArrayList<Carte> getPlisGagnes() {
         return plisGagnes;
     }
 
+    /**
+     * Met à jour les plis gagnés par le joueur
+     * 
+     * @param plisGagnes ArrayList<Carte> qui correspond aux cartes gagnées par le joueur
+     */
     public void setPlisGagnes(ArrayList<Carte> plisGagnes) {
         this.plisGagnes = plisGagnes;
     }
 
+    /**
+     * Retourne la partie à laquelle le joueur participe
+     * 
+     * @return Partie 
+     */
     public Partie getmPartie() {
         return mPartie;
     }
 
+    /**
+     * Met à jour la partie à laquelle le joueur participe
+     * 
+     * @param mPartie Partie
+     */
     public void setmPartie(Partie mPartie) {
         this.mPartie = mPartie;
     }
 
+    /**
+     * Indique si le joueur est une IA ou pas
+     * 
+     * @return boolean
+     */
     public boolean isIA() {
         return IA;
     }
 
+    /**
+     * Met à jour le booléen qui indique si le joueur est une IA
+     * 
+     * @param IA boolean
+     */
     public void setIA(boolean IA) {
         this.IA = IA;
     }
 
+    /**
+     * Redéfinition de la méthode equals pour Joueur
+     * Permet de vérifier que deux joueurs sont bien le même
+     * 
+     * @param o Le deuxième joueur à comparer
+     * @return boolean : true si les deux joueurs sont le même, false sinon
+     */
     @Override
     public boolean equals(Object o) {
         if (((Joueur) o).getNom().equals(this.nom) && ((Joueur) o).isIA() == this.IA) {
@@ -151,6 +296,12 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.1DAF6847-A28B-2D0E-5550-AA504F96E002]
     // </editor-fold> 
+    /**
+     * Méthode qui permet à un joueur de jouer une carte et l'enlève de ses cartes en main
+     * 
+     * @param carte Carte qui correspond à la Carte à jouer
+     * @return Carte qui correspond à la Carte jouée
+     */
     public Carte jouer(Carte carte) {
         cartesEnMain.remove(carte);
 
@@ -160,6 +311,12 @@ public class Joueur {
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.D5451F0E-CF8C-0CD3-7590-145DAA5B4521]
     // </editor-fold> 
+    /**
+     * Méthode qui permet à un joueur de piocher dans le tas de cartes de la partie
+     * 
+     * @param tasDeCartes LinkedList<Carte> qui correspond au tas de cartes de la partie
+     * @return Carte la carte que le joueur a piochée
+     */
     public Carte piocher(LinkedList<Carte> tasDeCartes) {
         /* Si le tas est vide */
         if (tasDeCartes.isEmpty()) {
